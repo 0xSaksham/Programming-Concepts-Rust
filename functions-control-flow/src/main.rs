5921741ef858568de98b9a259f2d88b88f860dd4
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-11-17 20:09:10 +0530

    Refactor control flow functions and add new ones with parameters
    This commit refactors the existing control flow functions in the
    `main.rs` file, adds a new function `parameter_function` that takes
    an `i32` parameter, and also adds a new function `two_parameter_function`
    that takes an `i32` value and a `char` unit label as parameters.

--- functions-control-flow/src/main.rs
+++ functions-control-flow/src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!\n");
 
     another_function();
+    parameter_function(6);
+    two_parameter_function(22,'Z');
 }
 
 fn another_function(){
-    println!("Another Function");
+    println!("Another Function\n");
+}
+
+fn parameter_function(x:i32){
+    println!("The value of x is {}\n", x);
+}
+
+fn two_parameter_function(value:i32, unit_label:char){
+    println!("Value:{value} and Unit:{unit_label}\n");
 }
