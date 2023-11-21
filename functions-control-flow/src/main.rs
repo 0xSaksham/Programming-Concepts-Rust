2582480527b7207803b57165f74affafa2a9a675
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-11-21 17:14:10 +0530

    Refactor code and add arrow function for plus one operation and statement/expression example.

--- functions-control-flow/src/main.rs
+++ functions-control-flow/src/main.rs
@@ -1,9 +1,28 @@
+// Arrow function with paranthesis
+
+fn plus_one(x: i32) -> i32{
+    x+1
+}
+
 fn main() {
     println!("Hello, world!\n");
 
     another_function();
     parameter_function(6);
     two_parameter_function(22,'Z');
+
+    // Statements and Expressions
+    
+    let y = { //Everything inside this is expression.
+        let x = 3; 
+        x + 10
+    };
+    println!("The value of y is: {y}\n");
+
+
+// Using that arrow function
+    let x = plus_one(4);
+    println!("x:{x}\n");
 }
 
 fn another_function(){
@@ -18,7 +37,8 @@ fn two_parameter_function(value:i32, unit_label:char){
     println!("Value:{value} and Unit:{unit_label}\n");
 }
 
-// Statement and Expressions 
+
+
 
 
 
